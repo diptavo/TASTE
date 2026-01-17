545d7d6827cea5db716a9a93b31bb7789419ca31
Debepsita <debepsitamukherjee@gmail.com>
2026-01-17 10:38:18 -0600

    Update TARIF.R

--- R/TARIF.R
+++ R/TARIF.R
@@ -18,7 +18,7 @@ harmonize_matrices <- function(matrices) {
 #' @param mi The maximum number of iterations for each instance of the JIVE algorithm.
 #' @param status A boolean indicating whether or not to give output showing the progress of the JIVE algorithm. If TRUE, the algorithm will print out updates about the number of iterations the algorithm is taking and the progress of the rank selection method, if applicable. If FALSE, the algorithms will give no printed output when run.
 
-TARIF.D=function(matrices,mi,status)
+TASTE.D=function(matrices,mi,status)
 {
   library(r.jive)
   rr=jive(matrices,maxiter=mi,showProgress=status)
@@ -33,7 +33,7 @@ TARIF.D=function(matrices,mi,status)
 #' @param lambda.v upper limit of the l-1 norm of the vector v a sparse loading vector, corresponding to columns of joint matrix.
 #' @param gamma step-size.
 
-TARIF.S=function(pro_name,jefs,lower,upper,lambda.v,gamma)
+TASTE.S=function(pro_name,jefs,lower,upper,gamma)
 {
   library(PMA)
   R=jefs[[2]] #rbind the list of matrices.
@@ -44,10 +44,10 @@ TARIF.S=function(pro_name,jefs,lower,upper,lambda.v,gamma)
   c2=upper
   for(i in 1:(jefs[[1]]))
   {
-
+    pp=length(n1)
     l=c2+1
     z=1
-    while (l<c1 || l>c2 && z< lambda.v) {
+    while (l<c1 || l>c2 && z< pp^0.25) {
       tt1=SPC(h_sub2,sumabsv=z, K=1)
       l=length(tt1$v[tt1$v!=0])
       z=z+gamma
@@ -63,7 +63,7 @@ TARIF.S=function(pro_name,jefs,lower,upper,lambda.v,gamma)
 }
 
 #' overall procedure
-TARIF=function(matrices,mi=20,status="False",lowl=30,upl=100,lambda.v,gamma)
+TASTE=function(matrices,mi=20,status="False",lowl=30,upl=100,gamma=0.1)
 {
   # Check if the input is a list of matrices
   if (!all(sapply(matrices, is.matrix))) {
@@ -79,6 +79,6 @@ TARIF=function(matrices,mi=20,status="False",lowl=30,upl=100,lambda.v,gamma)
 
   jx=TARIF.D(hm,mi,status)
 
-  protein_list=TARIF.S(colnames(hm[[1]]),jx,lowl,upl,,lambda.v,gamma)
+  protein_list=TARIF.S(colnames(hm[[1]]),jx,lowl,upl,gamma)
   return(protein_list)
 }
